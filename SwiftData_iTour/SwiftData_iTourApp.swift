fe56f78c5db5a0b998bac5c02412bebc306a9bb4
Evgeny Levin <evgenylevin91@gmail.com>
2023-10-16 18:43:37 +0200

    Initial Commit

---
+++ SwiftData_iTour/SwiftData_iTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftData_iTourApp.swift
+//  SwiftData_iTour
+//
+//  Created by YAUHENI LEVIN on 16.10.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftData_iTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
